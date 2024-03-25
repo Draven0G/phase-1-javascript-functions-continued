0c7268349a4cf46c0f66b2e42c2fa297d951a0c2
Ramsey <browenbroz@gmail.com>
2024-03-25 12:45:03 +0300

    js02

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(equal){
+   equal="This Saturday, I want to roller-skate!"
+    return("This Saturday, I want to bathe my dog!")
+}
+
+function mondayWork(){
+return("This Monday, I will go to the office.")
+}
+
+function wrapAdjective(adjective){
+    if('||'){
+        console.log("adjective-dazzles")
+    }
+}
